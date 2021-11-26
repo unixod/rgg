f1d50de10eac21950d5d660b465fcb9075be2301
Eldar Zakirov <unixod@gmail.com>
2021-11-26 22:57:53 +0300

    Update ez-utils and ez-support

--- deps-config.cmake
+++ deps-config.cmake
@@ -7,15 +7,14 @@ include(FetchContent)
 ###########################################
 FetchContent_Declare(ez-support
     GIT_REPOSITORY git@github.com:unixod/ez-support.git
-    GIT_TAG 3561b5d3ae027d90f9407c83760993979af4fdd1
+    GIT_TAG 090ec3b07569c87e3647698b4d0fcc67e73ffa18
     GIT_SHALLOW On
 )
 
 FetchContent_Declare(ez-utils
     GIT_REPOSITORY git@github.com:unixod/ez-utils.git
-    GIT_TAG 599bd48e55a96ab8573323b6573c30c3ffbcd4c2
+    GIT_TAG e98680bf7dd22a7596663e6a7b294ffde16e872d
     GIT_SHALLOW On
 )
-
 
 FetchContent_MakeAvailable(ez-support ez-utils)
